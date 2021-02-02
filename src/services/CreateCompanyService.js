56df804ccc1610db8c7d66429209c140d81d92d8
Matheus Costa <costamatheus97@live.com>
2021-02-01 21:19:06 -0300

    feat: added boilerplates to the services

--- src/services/CreateCompanyService.js
+++ src/services/CreateCompanyService.js
@@ -1,3 +1,8 @@
+const ContextInterface = require('../db/base/ContextInterface')
+const JobsRepository = require('../db/mongodb/repositories/JobsRepository')
+const CompanySchema = require('../db/mongodb/schemas/CompanySchema')
+const Base = require('../db/base/Base')
+
 class CreateCompanyService {
   async execute() {
 
--- src/services/CreateUserService.js
+++ src/services/CreateUserService.js
@@ -1,3 +1,8 @@
+const ContextInterface = require('../db/base/ContextInterface')
+const JobsRepository = require('../db/mongodb/repositories/JobsRepository')
+const UserSchema = require('../db/mongodb/schemas/UserSchema')
+const Base = require('../db/base/Base')
+
 class CreateUserService {
   async execute() {
 
